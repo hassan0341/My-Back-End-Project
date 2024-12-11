1c4d6f5e91f1fafcba73f77edfe16f50f4a66530
hassan0341 <hassan200354@outlook.com>
2024-12-11 15:27:21 +0000

    fix: firebase authentication

--- firebase-admin.js
+++ firebase-admin.js
@@ -1,5 +1,11 @@
 const admin = require("firebase-admin");
-const serviceAccount = require(process.env.FIREBASE_PRIVATE_KEY_PATH);
+let serviceAccount;
+
+if (process.env.NODE_ENV === "production") {
+  serviceAccount = JSON.parse(process.env.FIREBASE_PRIVATE_KEY);
+} else {
+  serviceAccount = require(process.env.FIREBASE_PRIVATE_KEY_PATH);
+}
 
 if (!admin.apps.length) {
   admin.initializeApp({
